2b9f2f05a2bf54b72939eef60032d6b800f6543f
DataM0del <183248792+DataM0del@users.noreply.github.com>
2025-11-07 15:40:21 -0600

    chore: add maven publish thing, and sources + javadoc jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     kotlin("jvm") version "2.2.0"
+    `maven-publish`
 }
+
+apply(plugin = "maven-publish")
 
 group = "test"
 version = "1.0-SNAPSHOT"
@@ -14,6 +17,22 @@ dependencies {
     implementation("org.glassfish.tyrus:tyrus-client:2.2.1")
     implementation("org.glassfish.tyrus:tyrus-container-grizzly-client:2.2.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+        }
+    }
 }
 
 kotlin {
