0d785a3fb0308932ba9a12087d0bc9be42c82472
DataM0del <183248792+DataM0del@users.noreply.github.com>
2025-11-07 12:13:36 -0600

    chore: 🤫

--- src/main/kotlin/rise/WebSocketClient.kt
+++ src/main/kotlin/rise/WebSocketClient.kt
@@ -16,9 +16,17 @@ import rise.packet.api.C2SPacket
 import rise.packet.api.S2CPacket
 import rise.packet.impl.c2s.community.asIRCMessage
 import rise.packet.impl.c2s.protection.C2SPacketAuthenticate
+import rise.packet.impl.s2c.community.S2CPacketCommunityInfo
 import rise.packet.impl.s2c.community.S2CPacketIRCMessage
+import rise.packet.impl.s2c.community.S2CPacketTabIRC
+import rise.packet.impl.s2c.community.S2CPacketTitleIRC
+import rise.packet.impl.s2c.community.S2CPacketTroll
 import rise.packet.impl.s2c.general.S2CPacketKeepAlive
+import rise.packet.impl.s2c.management.S2CPacketCrash
+import rise.packet.impl.s2c.protection.S2CPacketAltLogin
 import rise.packet.impl.s2c.protection.S2CPacketAuthenticationFinish
+import rise.packet.impl.s2c.protection.S2CPacketJoinServer
+import rise.packet.impl.s2c.protection.S2CPacketLoadConfig
 import java.net.URI
 import java.nio.ByteBuffer
 import kotlin.time.Duration.Companion.milliseconds
@@ -89,11 +97,14 @@ class WebSocketClient {
     }
 }
 
-fun connectAs(username: String) {
+
+@JvmRecord
+data class Account(val username: String, val hwid: String)
+
+fun connectAs(acc: Account) {
     val wsc = WebSocketClient()
-    val spamExempt = username == "billionaire"
     wsc.addHandshakeListener {
-        wsc.send(C2SPacketAuthenticate(username, "ok lol"))
+        wsc.send(C2SPacketAuthenticate(acc.username, "ok lol"))
     }
     wsc.addPacketListener { packet ->
         when (packet) {
@@ -101,15 +112,8 @@ fun connectAs(username: String) {
                 val msg = packet.message
                 // they append a number to it lol
                 val author = packet.author.slice(1..<packet.author.length)
+                // fun fact: you can use the mc colors in the message, and it'll be colored.
                 println("[IRC] $author: $msg")
-                CoroutineScope(Dispatchers.IO).launch {
-                    wsc.send("@$author Proof of $msg? ${(1..3).random()}".asIRCMessage)
-                    if (!spamExempt) delay(3.01e3.milliseconds)
-                    wsc.send("@$author 0".asIRCMessage)
-                    if (!spamExempt) delay(3.01e3.milliseconds)
-                    if (!spamExempt) wsc.send("@$author FOLD!".asIRCMessage)
-                    delay(3.01e3.milliseconds)
-                }
             }
             is S2CPacketAuthenticationFinish -> {
                 val success = packet.success
@@ -120,6 +124,30 @@ fun connectAs(username: String) {
                 println("Max Pitch: ${packet.maxPitch}")
             }
             is S2CPacketKeepAlive -> {}
+            is S2CPacketTabIRC -> {
+                println("Tab IRC packet: ${packet.data}")
+            }
+            is S2CPacketTitleIRC -> {
+                println("Title IRC packet: ${packet.message} with color ${packet.color}")
+            }
+            is S2CPacketCommunityInfo -> {
+                println("Community info with type ${packet.type}")
+            }
+            is S2CPacketJoinServer -> {
+                println("Join Server packet: ${packet.ip}:${packet.port}")
+            }
+            is S2CPacketTroll -> {
+                println("Troll packet: Reverse Binds ${packet.reverseKeybinds} Aura Disabled ${packet.killauraDisabled}")
+            }
+            is S2CPacketAltLogin -> {
+                println("Alt Login packet: ${packet.username} uuid ${packet.uuid} RT ${packet.refreshToken} AT ${packet.accessToken}")
+            }
+            is S2CPacketLoadConfig -> {
+                println("Got load config packet: ${packet.config}")
+            }
+            is S2CPacketCrash -> {
+                println("Ignoring crash packet")
+            }
             else -> {
                 println("Unhandled packet: $packet")
             }
@@ -129,9 +157,9 @@ fun connectAs(username: String) {
 }
 
 fun main() {
-    val namesToLock = setOf("billionaire", "billionaire2")
-    for (username in namesToLock) {
-        connectAs(username)
+    val accounts = setOf(Account("YourNameHere", "YourHWIDHere"))
+    for (acc in accounts) {
+        connectAs(acc)
     }
     println("Press ENTER / RETURN to stop.")
     readlnOrNull()
